950863ccdbb9f1948cc59bde0184804b3699b6d7
Tito Esteves <titoesteves@live.com>
2015-11-01 00:37:57 -0400

    added carousel and custom css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function() {
+  $('section#screenshots a').on('click', function() {
+    console.log('test')
+    $('div#modal img').attr('src', $(this).attr('data-image-url'));
+  });
+});
