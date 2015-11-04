d450ad6ca1e38ad5a94833c9f2d1dab1caf190ed
Tito Esteves <titoesteves@live.com>
2015-11-03 22:01:45 -0500

    added custom css for scroll animation

--- js/main.js
+++ js/main.js
@@ -3,4 +3,27 @@ $(document).ready(function() {
     console.log('test')
     $('div#modal img').attr('src', $(this).attr('data-image-url'));
   });
+
+  var nav = $('.navbar-fixed-top');
+  var distance = $('.navbar-fixed-top').offset();
+
+  if(distance.top >= 300){
+    if(scroll >= 300){
+      nav.addClass('effect');
+    }
+    else{
+      nav.removeClass('effect');
+    }
+  }
+
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+
+    if(scroll >= 300){
+      nav.addClass('effect');
+    }
+    else{
+      nav.removeClass('effect');
+    }
+  });
 });
